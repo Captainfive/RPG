61be9fd66bd41e915390d05e9a8f68721dcdc27c
Captainfive <vinou_montes@hotmail.fr>
2019-01-11 11:54:03 +0100

    docs: Creating documentation

--- scripts/save.js
+++ scripts/save.js
@@ -1,6 +1,13 @@
+// Require Node.js Dependencies
 const { readFile, writeFile } = require("fs").promises;
 
+/**
+ * @async
+ * @function
+ * @returns {Promise<void>}
+ */
 async function SAVE(data, path) {
+    // READ DATA AND WRITE IT
     const buf = await readFile(data);
     writeFile(path, buf);
 }
